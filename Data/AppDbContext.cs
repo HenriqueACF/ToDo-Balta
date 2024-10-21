ff6c7a0a5876ce2d888cb032852d069f12ab824d
henriqueacf <henrique.assis145@gmail.com>
2024-10-21 11:00:41 -0300

    feat: init dbContext

---
+++ Data/AppDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ToDo_Balta.Models;
+
+namespace ToDo_Balta.Data;
+
+public class AppDbContext :  DbContext
+{
+    public DbSet<ToDo> Todos { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) 
+        => optionsBuilder.UseSqlite("DataSource=app.db;Cache=Shared");
+    
+}
