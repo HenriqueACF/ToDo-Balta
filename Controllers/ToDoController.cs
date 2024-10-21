6cf5359a2f7ffe57da7fee7cd963d62d477751a2
henriqueacf <henrique.assis145@gmail.com>
2024-10-21 15:16:06 -0300

    feat: created HttpPost method in Controller

--- Controllers/ToDoController.cs
+++ Controllers/ToDoController.cs
@@ -2,6 +2,7 @@
 using Microsoft.EntityFrameworkCore;
 using ToDo_Balta.Data;
 using ToDo_Balta.Models;
+using ToDo_Balta.ViewModels;
 
 namespace ToDo_Balta.Controllers;
 
@@ -22,6 +23,7 @@ public class ToDoController : ControllerBase
         return Ok(todos);
     }
     
+    //PEGANDO POR ID
     [HttpGet]
     [Route("todos/{id}")]
     public async Task<IActionResult> GetByIdAsync(
@@ -34,5 +36,35 @@ public class ToDoController : ControllerBase
             .FirstOrDefaultAsync(x => x.Id == id);
 
         return todo == null ? NotFound() : Ok(todo);
+    }
+
+    //CRIANDO TODO
+    [HttpPost]
+    [Route("todos")]
+    public async Task<IActionResult> PostAsync(
+        [FromServices] AppDbContext context,
+        [FromBody] CreateTodoViewModel model)
+    {
+        if (!ModelState.IsValid)
+            return BadRequest();
+
+        var todo = new ToDo()
+        {
+            Date = DateTime.Now,
+            Done = false,
+            Title = model.Title
+        };
+
+        try
+        {
+            await context.Todos.AddAsync(todo);
+            await context.SaveChangesAsync();
+            return Created($"v1/todos/{todo.Id}", todo);
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e);
+            return BadRequest();
+        }
     }
 }
---
+++ ViewModels/CreateTodoViewModel.cs
@@ -0,0 +1,9 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ToDo_Balta.ViewModels;
+
+public class CreateTodoViewModel
+{
+    [Required]
+    public string Title { get; set; }
+}
