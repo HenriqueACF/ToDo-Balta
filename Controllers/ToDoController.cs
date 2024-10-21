8b00140645d5141390f01c0dafc64060731aeda5
henriqueacf <henrique.assis145@gmail.com>
2024-10-21 15:53:39 -0300

    feat: created HttpDelet method

--- Controllers/ToDoController.cs
+++ Controllers/ToDoController.cs
@@ -99,4 +99,28 @@ public class ToDoController : ControllerBase
             return BadRequest();
         }
     }
+    
+    //UPDATE TODO
+    [HttpDelete]
+    [Route("todos/{id}")]
+    public async Task<IActionResult> DeleteAsync(
+        [FromServices] AppDbContext context,
+        [FromRoute] int id)
+    {
+        var todo = await context
+            .Todos
+            .FirstOrDefaultAsync(x => x.Id == id);
+
+        try
+        {
+            context.Todos.Remove(todo);
+            await context.SaveChangesAsync();
+
+            return Ok();
+        }
+        catch (Exception e)
+        {
+            return BadRequest();
+        }
+    }
 }
