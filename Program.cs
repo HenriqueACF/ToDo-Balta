7a12e6fdf44a37beabc4fa0af9118a022c38d283
henriqueacf <henrique.assis145@gmail.com>
2024-10-21 11:33:13 -0300

    feat: configuring program.cs to read controller

--- Program.cs
+++ Program.cs
@@ -1,6 +1,12 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+// app.MapGet("/", () => "Hello World!");
+builder.Services.AddControllers();
+
+app.MapControllerRoute(
+    "default",
+    "{controller=Home}/{action=Index}/{id?}"
+);
 
 app.Run();
