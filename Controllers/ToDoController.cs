726d75e6e789e9bab67e96530e42c2e13c980028
henriqueacf <henrique.assis145@gmail.com>
2024-10-21 11:46:20 -0300

    feat: create controller

---
+++ Controllers/ToDoController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using ToDo_Balta.Data;
+using ToDo_Balta.Models;
+
+namespace ToDo_Balta.Controllers;
+
+[ApiController]
+[Route("v1")]
+public class ToDoController : ControllerBase
+{
+    [HttpGet]
+    [Route("todos")]
+    public async Task<IActionResult> Get([FromServices] AppDbContext context)
+    {
+        var todos = await context
+            .Todos
+            .AsNoTracking()    
+            .ToListAsync();
+        
+        return Ok(todos);
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,8 +1,12 @@
+using ToDo_Balta.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
 // app.MapGet("/", () => "Hello World!");
+//SERVICES
 builder.Services.AddControllers();
+builder.Services.AddDbContext<AppDbContext>();
 
 app.MapControllerRoute(
     "default",
