e6b2ebab5784a14d568cbf3b0741cca4f8825d38
henriqueacf <henrique.assis145@gmail.com>
2024-10-21 14:36:14 -0300

    feat: recuperando atraves do ID

--- Controllers/ToDoController.cs
+++ Controllers/ToDoController.cs
@@ -9,9 +9,10 @@ namespace ToDo_Balta.Controllers;
 [Route("v1")]
 public class ToDoController : ControllerBase
 {
+    // pegando todos os dados
     [HttpGet]
     [Route("todos")]
-    public async Task<IActionResult> Get([FromServices] AppDbContext context)
+    public async Task<IActionResult> GetAsync([FromServices] AppDbContext context)
     {
         var todos = await context
             .Todos
@@ -19,5 +20,19 @@ public class ToDoController : ControllerBase
             .ToListAsync();
         
         return Ok(todos);
+    }
+    
+    [HttpGet]
+    [Route("todos/{id}")]
+    public async Task<IActionResult> GetByIdAsync(
+        [FromServices] AppDbContext context,
+        [FromRoute] int id)
+    {
+        var todo = await context
+            .Todos
+            .AsNoTracking()    
+            .FirstOrDefaultAsync(x => x.Id == id);
+
+        return todo == null ? NotFound() : Ok(todo);
     }
 }
